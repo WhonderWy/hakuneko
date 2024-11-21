3f87e93a622478190204bb08f98bfb25b6fe349c
William [z5017279] <26218326+WhonderWy@users.noreply.github.com>
2024-11-21 22:38:41 +1100

    Fix futabanet.

--- src/web/mjs/connectors/Futabanet.mjs
+++ src/web/mjs/connectors/Futabanet.mjs
@@ -17,7 +17,7 @@ export default class Futabanet extends SpeedBinb {
 
     async _getMangaFromURI(uri) {
         let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.works__grid div.list__text div.mbOff h1');
+        data = await this.fetchDOM(request, 'div.works__list div.list__text-sp.mbOn p');
         let id = uri.pathname;
         let title = data[0].textContent.trim();
         return new Manga(this, id, title);
