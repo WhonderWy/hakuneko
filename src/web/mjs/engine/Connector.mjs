5c12633f35634d90fe945c79ab2e94a73ed45c0a
William [z5017279] <26218326+WhonderWy@users.noreply.github.com>
2025-02-28 00:41:40 +1100

    Improve code logic.

--- src/web/mjs/engine/Connector.mjs
+++ src/web/mjs/engine/Connector.mjs
@@ -431,16 +431,15 @@ export default class Connector {
             request = new Request(request.href, this.requestOptions);
         }
         const response = await fetch(request.clone());
-        const randomDelay = Math.abs(Math.random() + 1337 + Math.random() * 1337);
         if (retries === undefined || retries === null || retries > 0) {
             if (retries === undefined || retries === null) {
                 retries = 1;
             }
             if (response.status === 429) {
-                await this.wait(2500 + 8080 + randomDelay);
+                await this.wait(2500 + 8080);
                 return this.fetchDOM(request, selector, retries - 1);
             } else if (response.status >= 500) {
-                await this.wait(2500 + 1337 + randomDelay);
+                await this.wait(2500 + 1337);
                 return this.fetchDOM(request, selector, retries - 1);
             }
         }
