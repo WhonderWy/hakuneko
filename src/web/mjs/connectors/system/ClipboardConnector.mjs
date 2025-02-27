463568e9947b6307ebb989f3bfa4a9b00c6b02aa
William [z5017279] <26218326+WhonderWy@users.noreply.github.com>
2025-02-28 00:41:40 +1100

    Improve code logic.

--- src/web/mjs/connectors/system/ClipboardConnector.mjs
+++ src/web/mjs/connectors/system/ClipboardConnector.mjs
@@ -14,6 +14,7 @@ export default class ClipboardConnector {
         this.icon = '/img/connectors/' + this.id;
         this.tags = [];
         this.url = undefined;
+        this.urlsByDomain = undefined;
 
         this.clipboard = require('electron').clipboard;
         this.mangaCache = [];
@@ -23,14 +24,28 @@ export default class ClipboardConnector {
      *
      */
     updateMangas(callback) {
-        if(!this.isUpdating) {
+        if (!this.isUpdating) {
             this.isUpdating = true;
+            // this.setUrlsByDomain();
             this._getMangaList((_, mangas) => {
                 this.mangaCache = mangas;
                 this.isUpdating = false;
                 this.getMangas(callback);
             });
         }
+    }
+
+    setUrlsByDomain() {
+        let text = this.clipboard.readText();
+        this.urlsByDomain = !text ? { "N/A": [] } : text.split(/\r?\n/)
+            .reduce((acc, item) => {
+                const key = new URL(item).hostname;
+                if (!acc[key]) {
+                    acc[key] = [];
+                }
+                acc[key].push(item);
+                return acc;
+            }, {});
     }
 
     /**
@@ -45,24 +60,33 @@ export default class ClipboardConnector {
      */
     async _getMangaList(callback) {
         let text = this.clipboard.readText();
+        const connectorHits = {};
         let promises = !text ? [] : text.split(/\r?\n/).map(async line => {
             try {
                 let uri = new URL(line);
                 let connectors = Engine.Connectors.filter(connector => connector.canHandleURI && connector.canHandleURI(uri));
-                if(!connectors.length) {
+                if (!connectors.length) {
                     connectors = Engine.Connectors.filter(connector => connector.url && connector.url.includes(uri.hostname));
                 }
-                if(connectors.length > 1) {
+                if (connectors.length > 1) {
                     connectors = connectors.filter(connector => uri.pathname.startsWith(new URL(connector.url).pathname));
                 }
-                if(!connectors.length) {
+                if (!connectors.length) {
                     throw new Error('No matching connector found for URL ' + uri.href);
                 }
-                if(connectors.length > 1) {
+                if (connectors.length > 1) {
                     throw new Error('To many matching connectors found for URL ' + uri.href);
                 }
+                if (uri.hostname in connectorHits) {
+                    await new Promise(resolve => {
+                        setTimeout(resolve, Math.random() * 8080 + 1337 * connectorHits[uri.hostname]);
+                    });
+                } else {
+                    connectorHits[uri.hostname] = 0;
+                }
+                connectorHits[uri.hostname]++;
                 return await connectors[0].getMangaFromURI(uri);
-            } catch(error) {
+            } catch (error) {
                 console.warn('CLIPBOARD:', line, error);
                 return null;
             }
